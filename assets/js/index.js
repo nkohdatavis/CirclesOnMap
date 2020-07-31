68358e81efc8c17f3fea2d2811032fcf482d5c2c
nkohdatavis <nkohdatavis@gmail.com>
2020-07-31 16:42:11 +0000

    Add circles

--- assets/js/index.js
+++ assets/js/index.js
@@ -15,18 +15,21 @@ const svg = d3.select('svg');
 
 const projection = d3.geoNaturalEarth1();
 const pathGenerator = d3.geoPath().projection(projection);
+const radiusScale = d3.scaleSqrt();
+//accessor function
+const radiusValue = d => d.properties['2020'];
 
 const g = svg.append('g');
 
 const colorLegendG = svg.append('g')
-    .attr('transform', `translate(40,310)`);
+  .attr('transform', `translate(40,310)`);
 
 g.append('path')
-    .attr('class', 'sphere')
-    .attr('d', pathGenerator({type: 'Sphere'}));
+  .attr('class', 'sphere')
+  .attr('d', pathGenerator({ type: 'Sphere' }));
 
 svg.call(d3.zoom().on('zoom', () => {
-  g.attr('transform', event.transform);
+  g.attr('transform', d3.event.transform);
 }));
 
 // const colorScale = scaleOrdinal();
@@ -35,27 +38,39 @@ svg.call(d3.zoom().on('zoom', () => {
 // const colorValue = d => d.properties.economy;
 
 loadAndProcessData().then(countries => {
-  
+
+  radiusScale
+    .domain([0, d3.max(countries.features, radiusValue)])
+    .range([0, 20]);
+
   // colorScale
   //   .domain(countries.features.map(colorValue))
   //   .domain(colorScale.domain().sort().reverse())
   //   .range(schemeSpectral[colorScale.domain().length]);
-  
-//   colorLegendG.call(colorLegend, {
-//     colorScale,
-//     circleRadius: 8,
-//     spacing: 20,
-//     textOffset: 12,
-//     backgroundRectWidth: 235
-//   });
-  
+
+  //   colorLegendG.call(colorLegend, {
+  //     colorScale,
+  //     circleRadius: 8,
+  //     spacing: 20,
+  //     textOffset: 12,
+  //     backgroundRectWidth: 235
+  //   });
+
   g.selectAll('path').data(countries.features)
     .enter().append('path')
-      .attr('class', 'country')
-      .attr('d', pathGenerator)
-      .attr('fill', d => d.properties ? 'green' : 'red')
-      // .attr('fill', d => colorScale(colorValue(d)))
+    .attr('class', 'country')
+    .attr('d', pathGenerator)
+    .attr('fill', d => d.properties['2020'] ? 'green' : 'red')
+    // .attr('fill', d => colorScale(colorValue(d)))
     .append('title')
-      .text(d => d.properties.name + ': ' + colorValue(d));
-  
+    .text(d => d.id);
+  // ?    .text(d => d.properties.name + ': ' + colorValue(d));
+
+  g.selectAll('circle').data(countries.featuresWithPopulation)
+    .enter().append('circle')
+    .attr('class', 'country-circle')
+    //centroid
+    .attr('cx', d => projection(d3.geoCentroid(d))[0])
+    .attr('cy', d => projection(d3.geoCentroid(d))[1])
+    .attr('r', d => radiusScale(radiusValue(d)));
 });
--- assets/js/loadAndProcessData.js
+++ assets/js/loadAndProcessData.js
@@ -1,12 +1,12 @@
 // import { feature } from 'topojson';
 // import { csv, json } from 'd3';
 
-export const loadAndProcessData = () => 
+export const loadAndProcessData = () =>
   Promise
     .all([
       // tsv('https://unpkg.com/world-atlas@1.1.4/world/50m.tsv'),
       d3.csv('https://gist.githubusercontent.com/nkohdatavis/268451b90e82c0b5432107dd80825aa8/raw/un-population-estimates-medium.csv'),
-      d3.json('https://unpkg.com/world-atlas@1.1.4/world/50m.json')
+      d3.json('https://unpkg.com/visionscarto-world-atlas@0.0.6/world/50m.json')
     ])
     .then(([unData, topoJSONdata]) => {
       console.log(unData);
@@ -18,8 +18,18 @@ export const loadAndProcessData = () =>
       const countries = topojson.feature(topoJSONdata, topoJSONdata.objects.countries);
 
       countries.features.forEach(d => {
-        Object.assign(d.properties, rowById[d.id]);
+        Object.assign(d.properties, rowById[+d.id]);
       });
 
-      return countries;
+      const featuresWithPopulation = countries.features
+        .filter(d => d.properties['2020'])
+        .map(d => {
+          d.properties['2020'] = +d.properties['2020'].replace(/ /g, '');
+          return d;
+        });
+
+      return {
+        features: countries.features,
+        featuresWithPopulation
+      };
     });
